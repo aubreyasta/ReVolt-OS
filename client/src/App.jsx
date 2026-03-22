dec9e4127bb3220c1975c5dcc1988a39ef06d11e
Samudera Aubreyasta <saubreyasta@gmail.com>
2026-03-22 17:07:54 +0000

    Revert "Add WorkspacePage route to App component"
    This reverts commit 0e6470310178623e9a378e7b50623c87a1bf3f67.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,6 @@ import AuditPage from "./pages/AuditPage";
 import PassportPage from "./pages/PassportPage";
 import AssemblyPage from "./pages/AssemblyPage";
 import BlueprintPage from "./pages/BlueprintPage";
-import WorkspacePage from "./pages/WorkspacePage";
 
 export default function App() {
   return (
@@ -16,7 +15,6 @@ export default function App() {
         <Route path="/passport/:id" element={<PassportPage />} />
         <Route path="/assembly" element={<AssemblyPage />} />
         <Route path="/blueprint/:id" element={<BlueprintPage />} />
-        <Route path="/workspace" element={<WorkspacePage />} />
       </Routes>
     </BrowserRouter>
   );
